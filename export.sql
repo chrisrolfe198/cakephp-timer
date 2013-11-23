ab95a97c99c930ae3a36a89cf524880f46a49dd9
Christopher Rolfe <christopher.rolfe198@gmail.com>
2013-11-23 16:49:06 +0000

    Updating the structure - no records

--- export.sql
+++ export.sql
@@ -28,6 +28,7 @@ CREATE TABLE `timers` (
   `createdTimestamp` int(11) DEFAULT NULL,
   `startedTimestamp` int(11) DEFAULT NULL,
   `stoppedTimestamp` int(11) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -74,4 +75,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-11-23 15:26:43
+-- Dump completed on 2013-11-23 16:49:00
