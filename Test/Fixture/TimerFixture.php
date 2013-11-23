7db5c2131d1752a343f3cd233b97d3de7adcfac5
Christopher Rolfe <christopher.rolfe198@gmail.com>
2013-11-23 15:32:27 +0000

    Adding a base Fixture to run Unit tests against

---
+++ Test/Fixture/TimerFixture.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * TimerFixture
+ *
+ */
+class TimerFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 20, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
+		'createdTimestamp' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'startedTimestamp' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'stoppedTimestamp' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' 			   => 1,
+			'name' 			   => 'My first timer',
+			'createdTimestamp' => 1385164800,
+			'startedTimestamp' => '',
+			'stoppedTimestamp' => ''
+		),
+		array(
+			'id' 			   => 3,
+			'name' 			   => 'Eggs',
+			'createdTimestamp' => 1385164800,
+			'startedTimestamp' => strtotime("now"),
+			'stoppedTimestamp' => ''
+			),
+	);
+
+}
