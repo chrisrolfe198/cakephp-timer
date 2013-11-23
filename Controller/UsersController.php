475c23f895c06ea2ef017fa14ab494d5b65e34ef
Christopher Rolfe <christopher.rolfe198@gmail.com>
2013-11-23 16:56:46 +0000

    Fleshing out the skeleton giving a basic login, register, logout system

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -21,7 +21,16 @@ class UsersController extends AppController
 	 */
 	public function login()
 	{
-
+		// If the request data is post
+		if ($this->request->is('post')) {
+			// Try and log in with it
+			if ($this->Auth->login()) {
+				// If we login, redirect the user
+				$this->redirect($this->Auth->redirect());
+			}
+			// Otherwise flash an error message
+			$this->Session->setFlash('Incorrect login details');
+		}
 	}
 
 	/**
@@ -29,7 +38,19 @@ class UsersController extends AppController
 	 */
 	public function register()
 	{
-
+		// If the request is post
+		if ($this->request->is('post')) {
+			// Try and save the data
+			if ($this->User->save($this->request->data)) {
+				$this->Session->setFlash('Thank you for registering!');
+				// Add the new user id to the request data array so we can log the user in
+				$this->request->data['User'] = array_merge($this->request->data['User'], array('id' => $id));
+				// Log the new user in
+				$this->Auth->login($this->request->data['User']);
+				// and redirect them
+				$this->redirect($this->Auth->redirect());
+			}
+		}
 	}
 
 	/**
@@ -37,7 +58,7 @@ class UsersController extends AppController
 	 */
 	public function logout()
 	{
-
+		$this->redirect($this->Auth->logout());
 	}
 
 }
