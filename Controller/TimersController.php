46b84de332c690f899960bf703deac2c88082039
Christopher Rolfe <christopher.rolfe198@gmail.com>
2013-11-23 15:06:54 +0000

    Creating skeleton controllers

---
+++ Controller/TimersController.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Controller used to manage the timers in the application
+ */
+class TimersController extends AppController
+{
+
+	/**
+	 * Displays the timers the user currently has active
+	 */
+	public function index()
+	{
+
+	}
+
+	/**
+	 * Adds a new timer to the system
+	 */
+	public function add()
+	{
+
+	}
+
+	/**
+	 * Starts a timer
+	 * @param  integer $timerId The id of the timer to start
+	 */
+	public function start($timerId)
+	{
+
+	}
+
+	/**
+	 * Stops a timer
+	 * @param  integer $timerId The id of the timer to stop
+	 */
+	public function stop($timerId)
+	{
+
+	}
+}
---
+++ Controller/UsersController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Controller used to log users in, register users and log users out
+ */
+class UsersController extends AppController
+{
+
+	/**
+	 * Logs a user in to access their timers
+	 */
+	public function login()
+	{
+
+	}
+
+	/**
+	 * Registers a user to the system
+	 */
+	public function register()
+	{
+
+	}
+
+	/**
+	 * Deletes the users session, logging them out
+	 */
+	public function logout()
+	{
+
+	}
+
+}
